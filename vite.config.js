c7553091bb85002ba84ba1d60bd7c8b078ad14d0
Govind Kumawat <govind.kumawat@orderstack.one>
2024-09-21 11:00:00 +0530

    Other: vite config fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import topLevelAwait from "vite-plugin-top-level-await";
+// import topLevelAwait from "vite-plugin-top-level-await";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
